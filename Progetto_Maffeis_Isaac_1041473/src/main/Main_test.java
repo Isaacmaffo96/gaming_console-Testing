c8c17828a79578256b204d725cb1974e719b878b
Isaac <isaacmaffeis@gmail.com>
2023-08-02 00:33:16 +0200

    travis

--- Progetto_Maffeis_Isaac_1041473/src/main/Main_test.java
+++ Progetto_Maffeis_Isaac_1041473/src/main/Main_test.java
@@ -8,16 +8,10 @@ import login.SetupFacade;
 public class Main_test {
 
 	public static void main(String[] args) {
-		
-		//finds Java version    
-		String str=System.getProperty("java.version");  
-		//prints Java version  
-		System.out.println("Java Version is: "+str);    
-		
-				
-		//SetupFacade setupFacade = SetupFacade.createSetupFacade();
+		 	
+		SetupFacade setupFacade = SetupFacade.createSetupFacade();
 						
-		//LoginFrame loginFrame = new LoginFrame(setupFacade);
+		LoginFrame loginFrame = new LoginFrame(setupFacade);
 		// SignupFrame signupFrame = new SignupFrame(setupFacade.getCredentialsMap());
 		// GamesFrame gamesFrame = new GamesFrame(setupFacade.getCredentialsMap().get("isaacmaffo96"), setupFacade.getGamesList());
 			
